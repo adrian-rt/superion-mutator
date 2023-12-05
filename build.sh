0c029dad935c219483e3f6052c08d41a3bf25650
marvi3 <marvi3@gmx.net>
2023-12-05 14:38:21 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ make -j16
 
 cd js_parser
 
-for f in *.cpp; do g++ -I ../runtime/src/ -I ~/Downloads/afl++ -c $f -fPIC -std=c++11 -fpermissive -Wattributes; done
+for f in *.cpp; do g++ -I ../runtime/src/ -I [Path to AFLplusplus] -c $f -fPIC -std=c++11 -fpermissive -Wattributes; done
 
 g++  -shared -std=c++11 *.o ../dist/libantlr4-runtime.a  -o libTreeMutation.so 
 
